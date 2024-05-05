193c75881cc5e3ced79b2994a7d82c942f079781
AnilP. <anl.pirincoglu@gmail.com>
2024-05-05 09:41:30 +0300

    add findbyemail query to Customer Repository

--- src/main/java/enoca/challenge/shopping/repository/CustomerRepository.java
+++ src/main/java/enoca/challenge/shopping/repository/CustomerRepository.java
@@ -2,6 +2,11 @@ package enoca.challenge.shopping.repository;
 
 import enoca.challenge.shopping.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    @Query("SELECT c FROM Customer c WHERE c.email = :email")
+    Optional<Customer> findByEmail(String email);
 }
