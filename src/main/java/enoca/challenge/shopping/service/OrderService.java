f7fd9dd103979884ef4592cf3cd641dd029c609b
AnilP. <anl.pirincoglu@gmail.com>
2024-05-06 21:34:39 +0300

    add getOrder and createOrder methods, refactor placeOrder

--- src/main/java/enoca/challenge/shopping/service/OrderService.java
+++ src/main/java/enoca/challenge/shopping/service/OrderService.java
@@ -1,6 +1,8 @@
 package enoca.challenge.shopping.service;
 
+import enoca.challenge.shopping.dto.CartResponse;
 import enoca.challenge.shopping.dto.OrderResponse;
+import enoca.challenge.shopping.entity.Order;
 
 import java.util.List;
 
@@ -8,4 +10,7 @@ public interface OrderService {
 
     OrderResponse placeOrder(Long cart_id);
     List<OrderResponse> getAllOrdersForCustomer(Long customer_id);
+
+    Order getOrder(Long id);
+    Order createOrder(CartResponse cartResponse);
 }
--- src/main/java/enoca/challenge/shopping/service/impl/OrderServiceImpl.java
+++ src/main/java/enoca/challenge/shopping/service/impl/OrderServiceImpl.java
@@ -1,12 +1,15 @@
 package enoca.challenge.shopping.service.impl;
 
+import enoca.challenge.shopping.dto.CartResponse;
 import enoca.challenge.shopping.dto.OrderResponse;
 import enoca.challenge.shopping.entity.Cart;
+import enoca.challenge.shopping.entity.Order;
 import enoca.challenge.shopping.entity.Product;
 import enoca.challenge.shopping.exception.GlobalException;
 import enoca.challenge.shopping.repository.OrderRepository;
 import enoca.challenge.shopping.service.CartService;
 import enoca.challenge.shopping.service.CustomerService;
+import enoca.challenge.shopping.service.OrderItemService;
 import enoca.challenge.shopping.service.OrderService;
 import enoca.challenge.shopping.util.CartConverter;
 import enoca.challenge.shopping.util.OrderConverter;
@@ -14,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,20 +26,28 @@ public class OrderServiceImpl implements OrderService {
     private OrderRepository orderRepository;
     private CartService cartService;
     private CustomerService customerService;
+    private OrderItemService orderItemService;
 
     @Autowired
-    public OrderServiceImpl(OrderRepository orderRepository, CartService cartService, CustomerService customerService) {
+    public OrderServiceImpl(OrderRepository orderRepository, CartService cartService, CustomerService customerService, OrderItemService orderItemService) {
         this.orderRepository = orderRepository;
         this.cartService = cartService;
         this.customerService = customerService;
+        this.orderItemService = orderItemService;
     }
 
     @Override
     public OrderResponse placeOrder(Long cart_id) {
-        OrderResponse orderResponse = OrderConverter.orderToResponse(
-                orderRepository.save(
-                        CartConverter.cartToOrder(
-                                stockControl(cart_id))));
+        CartResponse cart = cartService.getCart(cart_id);
+        Order order = createOrder(cart);
+        order.getOrderItems().addAll(stockControl(cart_id)
+                .products()
+                .stream()
+                .map(productResponse ->
+                        orderItemService.createOrderItem(productResponse,order))
+                .toList());
+        OrderResponse orderResponse = OrderConverter
+                .orderToResponse(orderRepository.save(order));
         cartService.emptyCart(cart_id);
         return orderResponse;
     }
@@ -47,7 +59,21 @@ public class OrderServiceImpl implements OrderService {
                         .getOrders());
     }
 
-    private Cart stockControl(Long cart_id) {//TODO Mantık Hatası Var
+    @Override
+    public Order getOrder(Long id) {
+        return orderRepository.findById(id).orElseThrow(()->
+                new GlobalException("Order with given id is not exist: " + id, HttpStatus.NOT_FOUND)
+        );
+    }
+
+    @Override
+    public Order createOrder(CartResponse cartResponse) {
+        return orderRepository.save(new Order(cartResponse.totalPrice(),
+                customerService.findByEmail(cartResponse.customerEmail()),
+                new ArrayList<>()));
+    }
+
+    private CartResponse stockControl(Long cart_id) {//TODO Mantık Hatası Var
         Cart cart = cartService.findCart(cart_id);
         for (Product product : cart.getProducts()) {
             if (product.getStockQuantity() <= 0)
@@ -55,6 +81,6 @@ public class OrderServiceImpl implements OrderService {
         }
         cart.getProducts()
                 .forEach(product -> product.setStockQuantity(product.getStockQuantity() - 1));
-        return cart;
+        return CartConverter.cartToResponse(cart);
     }
 }
