a2b1cab72e4cbd18c032d23aa6ea1f3eec45494d
AnilP. <anl.pirincoglu@gmail.com>
2024-05-06 12:48:40 +0300

    add product to order item utility method

--- src/main/java/enoca/challenge/shopping/util/ProductConverter.java
+++ src/main/java/enoca/challenge/shopping/util/ProductConverter.java
@@ -1,17 +1,28 @@
 package enoca.challenge.shopping.util;
 
 import enoca.challenge.shopping.dto.ProductResponse;
+import enoca.challenge.shopping.entity.Order;
+import enoca.challenge.shopping.entity.OrderItem;
 import enoca.challenge.shopping.entity.Product;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductConverter {
-    public static ProductResponse productToResponse(Product product){
+    public static ProductResponse productToResponse(Product product) {
         return new ProductResponse(product.getName(), product.getPrice(), product.getStockQuantity());
     }
 
-    public static List<ProductResponse> productsToResponseList(List<Product> products){
+    public static List<ProductResponse> productsToResponseList(List<Product> products) {
         return products.stream().map(ProductConverter::productToResponse).toList();
+    }
+
+    public static OrderItem productToOrderItem(Product product, Order order) {
+        return new OrderItem(product.getName(), product.getPrice(), order);
+    }
+
+    public static List<OrderItem> productToOrderItemList(List<Product> products, Order order) {
+        return new ArrayList<>(products.stream().map(product -> productToOrderItem(product, order)).toList());
     }
 }
