5cc753c3a7796c9759385ef2f3ac4f360c204e9a
AnilP. <anl.pirincoglu@gmail.com>
2024-05-06 15:32:56 +0300

    add OrderItem entity

---
+++ src/main/java/enoca/challenge/shopping/entity/OrderItem.java
@@ -0,0 +1,26 @@
+package enoca.challenge.shopping.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order_item", schema = "enoca_challenge")
+public class OrderItem extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
