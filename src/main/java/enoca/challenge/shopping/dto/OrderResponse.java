c3861ced6ba8932eed8d0669b61d256b11d5b4f4
AnilP. <anl.pirincoglu@gmail.com>
2024-05-06 15:36:53 +0300

    refactor typo

--- src/main/java/enoca/challenge/shopping/dto/OrderResponse.java
+++ src/main/java/enoca/challenge/shopping/dto/OrderResponse.java
@@ -1,8 +1,6 @@
 package enoca.challenge.shopping.dto;
 
-import enoca.challenge.shopping.entity.OrderItem;
-
 import java.util.List;
 
-public record OrderResponse(String customerEmail, List<OrderItemReponse> products, Double totalPrice) {
+public record OrderResponse(String customerEmail, List<OrderItemResponse> products, Double totalPrice) {
 }
