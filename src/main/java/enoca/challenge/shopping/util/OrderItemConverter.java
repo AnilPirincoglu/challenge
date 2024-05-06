437230c35ad5a556c1216d5c3c4e4b23b46e62da
AnilP. <anl.pirincoglu@gmail.com>
2024-05-06 21:28:39 +0300

    refactored code

--- src/main/java/enoca/challenge/shopping/util/OrderItemConverter.java
+++ src/main/java/enoca/challenge/shopping/util/OrderItemConverter.java
@@ -1,9 +1,7 @@
 package enoca.challenge.shopping.util;
 
 import enoca.challenge.shopping.dto.OrderItemResponse;
-import enoca.challenge.shopping.entity.Order;
 import enoca.challenge.shopping.entity.OrderItem;
-import enoca.challenge.shopping.entity.Product;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,13 +13,5 @@ public class OrderItemConverter {
 
     public static List<OrderItemResponse> orderItemToResponseList(List<OrderItem> orderItems){
         return new ArrayList<>(orderItems.stream().map(OrderItemConverter::orderItemToResponse).toList());
-    }
-
-    public static OrderItem productToOrderItem(Product product) {
-        return new OrderItem(product.getName(), product.getPrice(),new Order());
-    }
-
-    public static List<OrderItem> productToOrderItemList(List<Product> products) {
-        return new ArrayList<>(products.stream().map(OrderItemConverter::productToOrderItem).toList());
     }
 }
