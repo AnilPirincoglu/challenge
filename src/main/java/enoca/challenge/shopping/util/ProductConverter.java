7cac2fe4c5ace07e2b558a23394c982dbeba5de9
AnilP. <anl.pirincoglu@gmail.com>
2024-05-16 22:37:20 +0300

    add request to product converter methods

--- src/main/java/enoca/challenge/shopping/util/ProductConverter.java
+++ src/main/java/enoca/challenge/shopping/util/ProductConverter.java
@@ -1,12 +1,10 @@
 package enoca.challenge.shopping.util;
 
-import enoca.challenge.shopping.dto.ProductResponse;
-import enoca.challenge.shopping.entity.Order;
-import enoca.challenge.shopping.entity.OrderItem;
+import enoca.challenge.shopping.dto.request.ProductRequest;
+import enoca.challenge.shopping.dto.response.ProductResponse;
 import enoca.challenge.shopping.entity.Product;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ProductConverter {
@@ -18,5 +16,23 @@ public class ProductConverter {
         return products.stream().map(ProductConverter::productToResponse).toList();
     }
 
+    public static Product requestToProduct(ProductRequest productRequest) {
+        return new Product(productRequest.name(),
+                productRequest.price(),
+                productRequest.stock(),
+                null);
+    }
 
+    public static Product requestToProduct(Product product, ProductRequest productRequest) {
+        if (productRequest.price() != null)
+            product.setPrice(productRequest.price());
+
+        if (productRequest.name() != null)
+            product.setName(productRequest.name());
+
+        if (productRequest.stock() != null)
+            product.setStockQuantity(productRequest.stock());
+
+        return product;
+    }
 }
