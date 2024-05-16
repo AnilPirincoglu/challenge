1827a104bde71db0177d2076da4483893cdeb198
AnilP. <anl.pirincoglu@gmail.com>
2024-05-16 22:33:27 +0300

    add Customer request converter

--- src/main/java/enoca/challenge/shopping/util/CustomerConverter.java
+++ src/main/java/enoca/challenge/shopping/util/CustomerConverter.java
@@ -1,9 +1,21 @@
 package enoca.challenge.shopping.util;
-import enoca.challenge.shopping.dto.CustomerResponse;
+
+import enoca.challenge.shopping.dto.request.CustomerRequest;
+import enoca.challenge.shopping.dto.response.CustomerResponse;
+import enoca.challenge.shopping.entity.Cart;
 import enoca.challenge.shopping.entity.Customer;
 
+import java.util.ArrayList;
+
 public class CustomerConverter {
-    public static CustomerResponse customerToResponse(Customer customer){
+    public static CustomerResponse customerToResponse(Customer customer) {
         return new CustomerResponse(customer.getFirstName(), customer.getEmail());
+    }
+
+    public static Customer requestToCustomer(CustomerRequest customerRequest) {
+        return new Customer(customerRequest.firstName(),
+                customerRequest.lastName(),
+                customerRequest.email(),
+                new Cart(0d, null, new ArrayList<>()), new ArrayList<>());
     }
 }
